99cd1eeecb8e5986811176e33046f0af7dbf7fa6
Yousef Saad <saadrslan95@gmail.com>
2026-07-09 00:58:14 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::stdin;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
@@ -5,4 +6,9 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+
+    println!("read from terminal ", command)
 }
