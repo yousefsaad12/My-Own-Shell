ab6db3b2cb270516c3b78e05358bdb6c106090e0
Yousef Saad <saadrslan95@gmail.com>
2026-07-09 01:01:01 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,5 @@ fn main() {
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
 
-    println!("{} : command not found", command.trim())
+    println!("{}: command not found", command.trim())
 }
