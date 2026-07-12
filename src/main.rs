7c1b2b49abfa5f12284c74207a17b518f4e63367
Yousef Saad <saadrslan95@gmail.com>
2026-07-12 15:22:58 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::env;
+use std::os::unix::fs::PermissionsExt;
+use std::path::PathBuf;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
@@ -7,9 +10,10 @@ fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-
+       
+        
         let mut line = String::new();
-
+        
         io::stdin().read_line(&mut line).unwrap();
         let line = line.trim();
         let (command, text) = match line.split_once(' ') {
@@ -18,15 +22,16 @@ fn main() {
         };
 
         if command == "type" {
-
-            if text == "echo" || text == "exit" || text == "type"
-            {
+            if text == "echo" || text == "exit" || text == "type" {
                 println!("{} is a shell builtin", text)
-            }
-            else {println!("{}: not found", text)};
-        }
-
-        else if command == "echo" {
+            } else {
+                match find_exe(text)
+                {
+                    Some(path) => println!("{} is {}", text, path.display()),
+                    None => println!("{}: not found", text)
+                }
+            };
+        } else if command == "echo" {
             println!("{}", text)
         } else if command == "exit" {
             return;
@@ -34,4 +39,21 @@ fn main() {
             println!("{}: command not found", command)
         }
     }
+}
+
+fn find_exe(cmd : & str) -> Option<PathBuf>{
+
+    let path_var = env::var("PATH").ok()?;
+    for dir in env::split_paths(&path_var)
+    {
+        let candidate = dir.join(cmd);
+        if candidate.is_file(){
+            if let Ok(metadata) = std::fs::metadata(&candidate){
+                if metadata.permissions().mode() & 0o111 != 0 {
+                    return Some(candidate)
+                }
+            }
+        }
+    }
+    None
 }
